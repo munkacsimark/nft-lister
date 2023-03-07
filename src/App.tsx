fe325d3b568d1753fcbf54c9da2640aac46ace82
Mark Munkacsi <markmunkacsi@protonmail.com>
2023-03-08 03:23:45 +0400

    Initial commit 🚀

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "./App.module.css";
+
+const App: FunctionComponent = () => {
+  return <div className={styles.cat}>🐈</div>;
+};
+
+export default App;
